4b71c3ecc944178d554200f0b75fc165784366b4
Luiz Tozati <luiztozati77@gmail.com>
2023-08-26 23:14:13 -0300

    feat: Adicionar tratamento de exceção para parâmetros negativos

--- LPOOII/tarefa01/src/Shapes.java
+++ LPOOII/tarefa01/src/Shapes.java
@@ -2,20 +2,30 @@ public class Shapes {
 
   public static void main(String[] args) throws Exception {
 
-
   }
 
   public static final double PI = 3.14159;
 
+  private static void verificaParametrosNegativos(double... valores){
+    for(double valor : valores){
+      if (valor < 0){
+        throw new RuntimeException("Parâmetros não podem ser negativos!");
+      }
+    }
+  }
+
   public static double calculaAreaRetangulo(double lado1, double lado2) {
+    verificaParametrosNegativos(lado1, lado2);
     return lado1 * lado2;
   }
 
   public static double calculaAreaCircunferencia(double raio) {
+    verificaParametrosNegativos(raio);
     return 2 * PI * raio;
   }
 
   public static double calculaAreaTriangulo(double base, double altura) {
+    verificaParametrosNegativos(base, altura);
     return (base * altura) / 2;
   }
 }
