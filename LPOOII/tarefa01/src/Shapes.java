db74f0cdf9c93a2233c736dd7c4576950fd23f24
Luiz Tozati <luiztozati77@gmail.com>
2023-08-26 23:36:44 -0300

    feat: Adicionar estrutura para obter os parâmetros do usuário via teclado

--- LPOOII/tarefa01/src/Shapes.java
+++ LPOOII/tarefa01/src/Shapes.java
@@ -1,14 +1,47 @@
+import java.util.Scanner;
+
 public class Shapes {
 
   public static void main(String[] args) throws Exception {
+    Scanner sc = new Scanner(System.in);
 
+    System.out.println("Informe a área que deseja calcular: \nR - Retângulo\nC - Circunferência\nT - Triângulo");
+    char op = sc.next().toUpperCase().charAt(0);
+
+    while (op != 'R' && op != 'C' && op != 'T') {
+      System.out.println("Informe uma opção válida R / C / T : ");
+      op = sc.next().toUpperCase().charAt(0);
+    }
+
+    switch (op) {
+      case 'R':
+        System.out.println("Informe os dois lados do retângulo um em cada linha: ");
+        double lado1 = sc.nextDouble();
+        double lado2 = sc.nextDouble();
+        System.out.printf("A área do retângulo é: %.1f", calculaAreaRetangulo(lado1, lado2));
+        break;
+
+      case 'C':
+        System.out.println("Informe o raio da circunferência: ");
+        double raio = sc.nextDouble();
+        System.out.printf("A área do triângulo é %.1f", calculaAreaCircunferencia(raio));
+        break;
+
+      case 'T':
+        System.out.println("Informe a base a a altura do triângulo um em cada linha: ");
+        double base = sc.nextDouble();
+        double altura = sc.nextDouble();
+        System.out.printf("A área do triângulo é %.1f", calculaAreaTriangulo(base, altura));
+        break;
+    }
+    sc.close();
   }
 
   public static final double PI = 3.14159;
 
-  private static void verificaParametrosNegativos(double... valores){
-    for(double valor : valores){
-      if (valor < 0){
+  private static void verificaParametrosNegativos(double... valores) {
+    for (double valor : valores) {
+      if (valor < 0) {
         throw new RuntimeException("Parâmetros não podem ser negativos!");
       }
     }
