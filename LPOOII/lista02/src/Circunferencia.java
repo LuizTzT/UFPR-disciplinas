f902c42aa2da004d021536fd3dd35c4cb50e8d4d
Luiz Tozati <luiztozati77@gmail.com>
2023-08-29 13:11:12 -0300

    feat: Adicionar classes Circunferencia, Retangulo, Triangulo e seus construtores

---
+++ LPOOII/lista02/src/Circunferencia.java
@@ -0,0 +1,7 @@
+public class Circunferencia {
+  private double raio;
+
+  public Circunferencia(double raio) {
+    this.raio = raio;
+  }
+}
---
+++ LPOOII/lista02/src/Retangulo.java
@@ -0,0 +1,9 @@
+public class Retangulo {
+  private double lado1;
+  private double lado2;
+
+  public Retangulo(double lado1, double lado2) {
+    this.lado1 = lado1;
+    this.lado2 = lado2;
+  }
+}
