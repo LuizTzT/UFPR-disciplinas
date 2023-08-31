27ed65e61c9bcddc663455c8255df80804042a22
Luiz Tozati <luiztozati77@gmail.com>
2023-08-30 22:40:11 -0300

    feat: Adicionar método para calculo da area da Circunferencia

--- LPOOII/lista02/src/Circunferencia.java
+++ LPOOII/lista02/src/Circunferencia.java
@@ -1,7 +1,12 @@
 public class Circunferencia {
+
   private double raio;
 
   public Circunferencia(double raio) {
     this.raio = raio;
+  }
+
+  public double area() {
+    return Math.PI * Math.pow(raio, 2);
   }
 }
