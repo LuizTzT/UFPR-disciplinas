548e3d980106d9e4ac3aeb832693a4bb164e0c3b
Luiz Tozati <luiztozati77@gmail.com>
2023-08-30 22:37:48 -0300

    test: Adicionar testes de area para a classe Circunferencia

--- LPOOII/lista02/src/ShapesTest.java
+++ LPOOII/lista02/src/ShapesTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 public class ShapesTest {
 
   @Test
-  public void testArea() {
+  public void testAreaRetangulo() {
     Retangulo retangulo1 = new Retangulo(5.0, 10.0);
     double areaEsperada1 = retangulo1.area();
     double areaAtual1 = 50.0;
@@ -32,5 +32,20 @@ public class ShapesTest {
     double perimetroEsperado2 = (5.0 + 7.0) * 2;
 
     assertEquals(perimetroAtual2, perimetroEsperado2, 0.001);
+  }
+
+  @Test
+  public void testAreaCircunferencia() {
+    Circunferencia circunferencia1 = new Circunferencia(15);
+    double areaEsperada1 = circunferencia1.area();
+    double areaAtual1 = Math.PI * Math.pow(15, 2);
+
+    assertEquals(areaAtual1, areaEsperada1, 0.001);
+
+    Circunferencia circunferencia2 = new Circunferencia(10);
+    double areaEsperada2 = circunferencia2.area();
+    double areaAtual2 = Math.PI * Math.pow(10, 2);
+
+    assertEquals(areaAtual2, areaEsperada2, 0.001);
   }
 }
