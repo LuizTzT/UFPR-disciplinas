65e0eab534b254df613d54cfce66bbc3b48b90d4
Luiz Tozati <luiztozati77@gmail.com>
2023-09-01 23:24:58 -0300

    feat: Adicionar validação dos parâmetros e métodos getBase e getAltura à classe Triangulo

--- LPOOII/lista02/src/Triangulo.java
+++ LPOOII/lista02/src/Triangulo.java
@@ -3,11 +3,22 @@ public class Triangulo {
   private double altura;
 
   public Triangulo(double base, double altura) {
+    Validation.validarParametros(base, "A base do triângulo informada é negativa!");
+    Validation.validarParametros(altura, "A altura triângulo informada é negativa!");
     this.base = base;
     this.altura = altura;
   }
 
-  public double area(){
+  public double area() {
     return (base * altura) / 2;
   }
+
+  public double getBase() {
+    return base;
+  }
+
+  public double getAltura() {
+    return altura;
+  }
+
 }
