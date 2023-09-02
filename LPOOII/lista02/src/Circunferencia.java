1def14f06dd2820b8ad0e255bc3de4b338db3349
Luiz Tozati <luiztozati77@gmail.com>
2023-09-01 23:23:23 -0300

    feat: Adicionar validação dos parâmetros e método getRaio à classe Circunferência

--- LPOOII/lista02/src/Circunferencia.java
+++ LPOOII/lista02/src/Circunferencia.java
@@ -3,10 +3,15 @@ public class Circunferencia {
   private double raio;
 
   public Circunferencia(double raio) {
+    Validation.validarParametros(raio, "O raio informado é negativo!");
     this.raio = raio;
   }
 
   public double area() {
     return Math.PI * Math.pow(raio, 2);
+  }
+
+  public double getRaio() {
+    return raio;
   }
 }
