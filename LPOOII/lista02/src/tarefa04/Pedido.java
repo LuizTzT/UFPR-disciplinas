81d8b4411c823aeaca1d04fed9453bd0a93037dc
Luiz Tozati <luiztozati77@gmail.com>
2023-09-03 02:49:52 -0300

    feat: Adicionar métodos acrescentaItem e retiraItem

--- LPOOII/lista02/src/tarefa04/Pedido.java
+++ LPOOII/lista02/src/tarefa04/Pedido.java
@@ -30,5 +30,55 @@ public class Pedido {
     return somaTotal;
   }
 
- 
+  public void acrescentaItem(ItemDePedido item) {
+    try {
+
+      if (item == null) {
+        throw new ItemExceptions("Item nulo.");
+      } else if (getTotal() > valorTotalPermitido) {
+        throw new ItemExceptions("Item de Pedido não incluído. Valor de pedido excedido.");
+      } else if (valorTotalPermitido == 0) {
+        throw new ItemExceptions("Cliente não tem nenhum crédito");
+      } else {
+
+        boolean itemEncontrado = false;
+        for (ItemDePedido itemLista : itens) {
+          if (itemLista.getNome().equals(item.getNome())) {
+            itemEncontrado = true;
+            itemLista.setQuantidade(itemLista.getQuantidade() + item.getQuantidade());
+            return; // coloquei depois
+          }
+        }
+
+        if (!itemEncontrado) {
+          itens.add(item);
+        }
+      }
+
+    } catch (ItemExceptions e) {
+      System.out.println(e.getMessage());
+    }
+  }
+
+  public void retiraItem(String nome) {
+    boolean itemEncontrado = false;
+
+    try {
+
+      for (ItemDePedido item : itens) {
+        if (item.getNome().equals(nome)) {
+          itens.remove(item);
+          itemEncontrado = true;
+          break;
+        }
+      }
+
+      if (!itemEncontrado) {
+        throw new ItemExceptions("Item não encontrado no pedido.");
+      }
+    } catch (ItemExceptions e) {
+      System.out.println(e.getMessage());
+    }
+  }
+
 }
