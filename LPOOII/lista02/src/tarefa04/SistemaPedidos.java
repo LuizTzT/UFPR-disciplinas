71f4a4548135542fb1937049bc3db3434553bc9f
Luiz Tozati <luiztozati77@gmail.com>
2023-09-03 02:56:37 -0300

    feat: Adicionar Classe SistemaDePedidos e um menu de opções

---
+++ LPOOII/lista02/src/tarefa04/SistemaPedidos.java
@@ -0,0 +1,41 @@
+package tarefa04;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class SistemaPedidos {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    List<Pedido> pedidos = new ArrayList<>();
+
+    System.out.println("Informe qual ação deseja realizar: ");
+    System.out.println("1 - Incluir Pedido.");
+    System.out.println("2 - Excluir pedido por nome do cliente.");
+    System.out.println("3 - Listar Pedidos.");
+    System.out.println("4 - Incluir Item de Pedido em Pedido.");
+    System.out.println("5 - Excluir item de Pedido em Pedido.");
+    System.out.println("6 - Imprimir a lista de Pedidos.");
+    int op = sc.nextInt();
+
+    switch (op) {
+      case 1:
+        break;
+      case 2:
+        break;
+      case 3:
+        break;
+      case 4:
+        break;
+      case 5:
+        break;
+      case 6:
+        break;
+    }
+
+    sc.close();
+  }
+
+
+
+}
