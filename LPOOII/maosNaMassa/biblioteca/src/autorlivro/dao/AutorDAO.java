d7ebffe60c3e800ebbea64727d423c7291ae6cc9
Luiz Tozati <luiztozati77@gmail.com>
2023-09-05 23:29:39 -0300

    feat: Adicionar método adicionar para inserir autor no banco de dados

--- LPOOII/maosNaMassa/biblioteca/src/autorlivro/dao/AutorDAO.java
+++ LPOOII/maosNaMassa/biblioteca/src/autorlivro/dao/AutorDAO.java
@@ -1,5 +1,30 @@
 package autorlivro.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import autorlivro.entidade.Autor;
+import autorlivro.util.ConnectionDatabase;
+
 public class AutorDAO {
-  
+  private String sql = "INSERT INTO autor (nome) VALUES(?)";
+
+  public void adicionar(Autor autor) throws SQLException {
+    Connection connection = null;
+    PreparedStatement ps = null;
+
+    try {
+      connection = new ConnectionDatabase().getConnection();
+      ps = connection.prepareStatement(sql);
+
+      ps.setString(1, autor.getNome());
+      ps.execute();
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    } finally {
+      ps.close();
+      connection.close();
+    }
+  }
 }
