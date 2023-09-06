aea3c5d5937c3d37c756a0497eac459a7b9419b8
Luiz Tozati <luiztozati77@gmail.com>
2023-09-05 22:27:51 -0300

    feat: Adicionar Getters and Setters as classes Autor e Livro

--- LPOOII/maosNaMassa/biblioteca/src/autorlivro/Autor.java
+++ LPOOII/maosNaMassa/biblioteca/src/autorlivro/Autor.java
@@ -1,9 +1,33 @@
 package autorlivro;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Autor {
   private String nome;
+  private Set<Livro> livros;
 
   public Autor(String nome) {
     this.nome = nome;
+    this.livros = new HashSet<>();
   }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public Set<Livro> getLivros() {
+    return livros;
+  }
+
+  public void setLivros(Set<Livro> livros) {
+    this.livros = livros;
+  }
+
+  
+
 }
--- LPOOII/maosNaMassa/biblioteca/src/autorlivro/Livro.java
+++ LPOOII/maosNaMassa/biblioteca/src/autorlivro/Livro.java
@@ -4,10 +4,28 @@ import java.util.Set;
 
 public class Livro {
   private String titulo;
-  private Set<String> autores;
+  private Set<Autor> autores;
 
-  public Livro(String titulo, Set<String> autores) {
+  public Livro(String titulo, Set<Autor> autores) {
     this.titulo = titulo;
     this.autores = autores;
   }
+
+  public String getTitulo() {
+    return titulo;
+  }
+
+  public void setTitulo(String titulo) {
+    this.titulo = titulo;
+  }
+
+  public Set<Autor> getAutores() {
+    return autores;
+  }
+
+  public void setAutores(Set<Autor> autores) {
+    this.autores = autores;
+  }
+
+  
 }
