c2aa6e188496f3454eeac3e290b4bb0eb40fd8fa
Luiz Tozati <luiztozati77@gmail.com>
2023-09-06 18:04:21 -0300

    feat: Adicionar implementação do método insere

--- LPOOII/lista03/src/tarefa05/ContatoListDAO.java
+++ LPOOII/lista03/src/tarefa05/ContatoListDAO.java
@@ -13,6 +13,7 @@ public class ContatoListDAO {
   private String sqlAltera = "UPDATE contatos SET nome = ?, email = ?, endereco = ?, dataNascimento = ? WHERE id = ?";
   private String sqlRemove = "DELETE FROM contatos WHERE id = ?";
   private String sqlList = "SELECT * FROM contatos";
+  private String sqlInsere = "INSERT INTO contatos (id, nome, email, endereco, dataNascimento) VALUES(?,?,?,?,?)";
 
   // public void altera(Contato contato)
   public void altera(Contato contato) throws SQLException {
@@ -116,4 +117,31 @@ public class ContatoListDAO {
   }
 
   // public void insere(Contato contato)
+  public void insere(Contato contato) throws SQLException {
+    Connection connection = null;
+    PreparedStatement pstmt = null;
+
+    try {
+      connection = new ConnectionFactoryDatabase().getConnection();
+      pstmt = connection.prepareStatement(sqlInsere);
+
+      pstmt.setLong(1, contato.getId());
+      pstmt.setString(2, contato.getNome());
+      pstmt.setString(3, contato.getEmail());
+      pstmt.setString(4, contato.getEndereco());
+      pstmt.setDate(5, new Date(contato.getDataNascimento().getTimeInMillis()));
+
+      pstmt.executeUpdate();
+
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    } finally {
+      if (pstmt != null) {
+        pstmt.close();
+      }
+      if (connection != null) {
+        connection.close();
+      }
+    }
+  }
 }
