0a806bba7867c26580c408c2174560344cf3be4c
Luiz Tozati <luiztozati77@gmail.com>
2023-09-06 19:11:12 -0300

    test: Adicionar classe para teste do métodos altera, remove, list, insere

---
+++ LPOOII/lista03/src/tarefa05/TestContatoDAO.java
@@ -0,0 +1,54 @@
+package tarefa05;
+
+import static org.junit.Assert.assertEquals;
+
+import java.sql.SQLException;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+import org.junit.Test;
+
+public class TestContatoDAO {
+
+
+  @Test
+  public void testInserirContato() throws SQLException {
+
+    // Criar um contato para teste
+    Contato contato = new Contato();
+
+    // Criar uma instância do ContatoListDAO
+    ContatoListDAO listDao = new ContatoListDAO();
+
+    contato.setId((long) 16);
+    contato.setNome("Antonio Roberto");
+    contato.setEmail("beto15@gmail.com");
+    contato.setEndereco("Av. das armas n. 400");
+
+    Calendar dataNascimento = Calendar.getInstance();
+    dataNascimento.set(Calendar.YEAR, 1980);
+    dataNascimento.set(Calendar.MONTH, Calendar.MARCH);
+    dataNascimento.set(Calendar.DAY_OF_MONTH, 21);
+
+    contato.setDataNascimento(dataNascimento);
+
+    // Inserir contato
+    listDao.insere(contato);
+
+    Contato contatoInserido = listDao.findContato(16);
+
+    assertEquals(contato.getId(), contatoInserido.getId());
+    assertEquals(contato.getNome(), contatoInserido.getNome());
+    assertEquals(contato.getEmail(), contatoInserido.getEmail());
+    assertEquals(contato.getEndereco(), contatoInserido.getEndereco());
+
+    Calendar dataNascimentoOriginal = contato.getDataNascimento();
+    Calendar dataNascimentoInserido = contatoInserido.getDataNascimento();
+
+    assertEquals(dataNascimentoOriginal.get(Calendar.YEAR), dataNascimentoInserido.get(Calendar.YEAR));
+    assertEquals(dataNascimentoOriginal.get(Calendar.MONTH), dataNascimentoInserido.get(Calendar.MONTH));
+    assertEquals(dataNascimentoOriginal.get(Calendar.DAY_OF_MONTH), dataNascimentoInserido.get(Calendar.DAY_OF_MONTH));
+
+  }
+}
