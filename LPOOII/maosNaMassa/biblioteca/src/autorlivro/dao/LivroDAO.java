7df93f105b056c1fdd1fe1081d1cd9d60d35a24b
Luiz Tozati <luiztozati77@gmail.com>
2023-09-05 23:30:48 -0300

    feat: Adicionar método adicionar para inserir livro no banco de dados

--- LPOOII/maosNaMassa/biblioteca/src/autorlivro/dao/LivroDAO.java
+++ LPOOII/maosNaMassa/biblioteca/src/autorlivro/dao/LivroDAO.java
@@ -1,5 +1,30 @@
 package autorlivro.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import autorlivro.entidade.Livro;
+import autorlivro.util.ConnectionDatabase;
+
 public class LivroDAO {
-  
+  private String sql = "INSERT INTO livro (livro) VALUES(?)";
+
+  public void adicionar(Livro livro) throws SQLException {
+    Connection connection = null;
+    PreparedStatement ps = null;
+
+    try {
+      connection = new ConnectionDatabase().getConnection();
+      ps = connection.prepareStatement(sql);
+
+      ps.setString(1, livro.getTitulo());
+      ps.execute();
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    } finally {
+      ps.close();
+      connection.close();
+    }
+  }
 }
